054ea76907f5762f7003c5fa5e44e8e35b21b616
klp <2329831033@qq.com>
2020-12-07 09:04:21 +0800

    大事件Day1提交

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,10 +1,37 @@
-$(function (){
-    $('#link_reg').on('click',function () {
+$(function() {
+    $('#link_reg').on('click', function() {
         $('.login-box').hide();
         $('.reg-box').show();
     })
-    $('#link_login').on('click',function () {
+    $('#link_login').on('click', function() {
         $('.login-box').show();
         $('.reg-box').hide();
+    })
+
+    //从 layui 获取 form 对象
+    var form = layui.form
+        //通过lay-verify（）自定义效验规则
+    form.verify({
+            //自定义一个pwsd效验规则
+            pwsd: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'],
+            // 校验两次密码是否一致
+            repwd: function(value) {
+                var pwd = $('.reg-box [name=password]').val();
+                if (pwd !== value) {
+                    return '两次输入的密码不一致'
+                }
+            }
+        })
+        // 监听注册事件的提交事件
+    $('#form_reg').on('submit', function(e) {
+        e.preventDefault();
+        $.post('http://ajax.frontend.itheima.net/api/reguser', { username: $('#form_reg [name=username]').val(), password: $('#form_reg [name=password]').val() }, function(res) {
+            if (res.status !== 0) {
+                return console.log(res.message);
+            }
+            console.log('注册成功');
+            $('.login-box').show();
+            $('.reg-box').hide();
+        })
     })
 })
